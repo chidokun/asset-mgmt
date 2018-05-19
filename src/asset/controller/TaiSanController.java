feaa91344ee0a01a31bf8e49e6980afef5194c1f
hoanghouit <hohoang.ag.96@gmail.com>
2018-05-19 19:25:16 +0700

    Handle search asset with database

--- src/asset/controller/TaiSanController.java
+++ src/asset/controller/TaiSanController.java
@@ -1,16 +1,21 @@
 package asset.controller;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+
 import asset.entity.TaiSan;
 import asset.model.TaiSanModel;
 
 public class TaiSanController {
-	public static TaiSan select(String maTS) {
-		return TaiSanModel.select(maTS);
+	public static TaiSan select(String maTS, String maPN) throws SQLException {
+		return TaiSanModel.select(maTS, maPN);
 	}
 
-	public static ArrayList<TaiSan> search(String key) {
-		;
+	public static ArrayList<TaiSan> search(String key) throws SQLException {
 		return TaiSanModel.search(key);
+	}
+	
+	public static ArrayList<TaiSan> selectAll() throws SQLException {
+		return TaiSanModel.selectAll();
 	}
 }
--- src/asset/model/TaiSanModel.java
+++ src/asset/model/TaiSanModel.java
@@ -1,41 +1,64 @@
 package asset.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import asset.entity.TaiSan;
+import asset.util.Database;
 
 public class TaiSanModel {
 
-	public static TaiSan select(String maTS) {
-		// hard code
-		TaiSan ts = new TaiSan();
-		ts.setMaTS(maTS);
-		ts.setNgaySD(new Date(System.currentTimeMillis()));
-		ts.setSoNamKH(3);
-		ts.setSoThangSD(36);
-		ts.setTenTS("Màn hình laptop");
+	/**
+	 * Select an asset
+	 * 
+	 * @param maTS
+	 * @return
+	 * @throws SQLException
+	 */
+	public static TaiSan select(String maTS, String maPN) throws SQLException {
+		ResultSet rs = Database.callStored("sp_LayThongTin_TaiSan", maTS, maPN);
+
+		rs.next();
+		TaiSan ts = new TaiSan(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDate(4), rs.getInt(5),
+				rs.getString(6), rs.getInt(7), rs.getInt(8), rs.getString(9), rs.getDate(10));
+		Database.connect().close();
 		return ts;
 	}
 
-	public static ArrayList<TaiSan> search(String key) {
-		// hard code
-		ArrayList<TaiSan> arr = new ArrayList<TaiSan>();
-		TaiSan ts = new TaiSan();
-		ts.setMaTS("TS0001");
-		ts.setNgaySD(new Date(System.currentTimeMillis()));
-		ts.setSoNamKH(3);
-		ts.setSoThangSD(36);
-		ts.setTenTS("Màn hình laptop");
+	/**
+	 * Search asset
+	 * 
+	 * @return array list of asset
+	 * @throws SQLException
+	 */
+	public static ArrayList<TaiSan> search(String key) throws SQLException {
+		ResultSet rs = Database.callStored("sp_TraCuuTaiSan", key);
 
-		TaiSan ts1 = new TaiSan();
-		ts1.setMaTS("TS0002");
-		ts1.setNgaySD(new Date(System.currentTimeMillis()));
-		ts1.setSoNamKH(10);
-		ts1.setSoThangSD(16);
-		ts1.setTenTS("CPU core I7");
-		arr.add(ts);
-		arr.add(ts1);
-		;
+		ArrayList<TaiSan> arr = new ArrayList<>();
+		while (rs.next()) {
+			arr.add(new TaiSan(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDate(4), rs.getInt(5), rs.getString(6), rs.getDate(7)));
+		}
+
+		Database.connect().close();
+		return arr;
+	}
+
+	/**
+	 * Select all asset
+	 * 
+	 * @return array list of asset
+	 * @throws SQLException
+	 */
+	public static ArrayList<TaiSan> selectAll() throws SQLException {
+		ResultSet rs = Database.callStored("sp_TraCuuTaiSan", "");
+
+		ArrayList<TaiSan> arr = new ArrayList<>();
+		while (rs.next()) {
+			arr.add(new TaiSan(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDate(4), rs.getInt(5), rs.getString(6), rs.getDate(7)));
+		}
+
+		Database.connect().close();
 		return arr;
 	}
 }
